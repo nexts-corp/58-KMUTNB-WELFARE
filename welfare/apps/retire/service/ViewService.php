4f7b70e8e109f9c55038533ab89833128428022c
สิทธิพร <@bebear>
2015-08-24 18:54:41 +0700

    Revert "ปรับแก้เมนู"
    This reverts commit 11893ea5093938cb1eeb1706be98ee2cccf67ac0.

--- welfare/apps/retire/service/ViewService.php
+++ welfare/apps/retire/service/ViewService.php
@@ -21,30 +21,29 @@ class ViewService extends CServiceBase implements IViewService {
     public function retireList() {
         $view = new CJView("retire/lists", CJViewType::HTML_VIEW_ENGINE);
 
-        $sql = "SELECT concat(ti.titleNameTh,' ',re.registerNameTh,' ',re.registerLastNameTh) as fullname ,de.departmentNameTh,
-re.registerDateofBirth + interval 60 year as dateOut,
-CASE
-    WHEN re.registerDateAdded + interval 10 year >= curdate() THEN 10000
-    WHEN re.registerDateAdded + interval 15 year >= curdate() THEN 15000
-    WHEN re.registerDateAdded + interval 20 year >= curdate() THEN 20000
-    WHEN re.registerDateAdded + interval 25 year >= curdate() THEN 25000
-    WHEN re.registerDateAdded + interval 30 year >= curdate() THEN 30000
-    ELSE NULL
-END AS amount
-FROM register re
-left join department de on re.departmentId = de.departmentId
-left join titlename ti on re.titleNameId = ti.titleNameId
-where re.registerDateofBirth + interval 60 year >= curdate() GROUP BY re.registerId";
-        
-        $info = $this->datacontext->pdoQuery($sql);
-        if ($info) {
-            $view->retire = $info;
-            return $view;
-        }
-        else{
+//        $sql = "SELECT concat(ti.titleNameTh,' ',re.registerNameTh,' ',re.registerLastNameTh) as fullname ,de.departmentNameTh,
+//re.registerDateofBirth + interval 60 year as dateOut,
+//CASE
+//    WHEN re.registerDateAdded + interval 10 year >= curdate() THEN 10000
+//    WHEN re.registerDateAdded + interval 15 year >= curdate() THEN 15000
+//    WHEN re.registerDateAdded + interval 20 year >= curdate() THEN 20000
+//    WHEN re.registerDateAdded + interval 25 year >= curdate() THEN 25000
+//    WHEN re.registerDateAdded + interval 30 year >= curdate() THEN 30000
+//    ELSE NULL
+//END AS amount
+//FROM register re
+//left join department de on re.departmentId = de.departmentId
+//left join titlename ti on re.titleNameId = ti.titleNameId
+//where re.registerDateofBirth + interval 60 year >= curdate()";
+//        $info = $this->datacontext->pdoQuery($sql);
+//        if ($info) {
+//            $view->retire = $info;
+//            return $view;
+//        }
+//        else{}
             return $view;
           
-        }
+        
     }
 
 }
