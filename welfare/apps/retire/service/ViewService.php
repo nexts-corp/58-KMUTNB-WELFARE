8b4d91c243a368b8cf643ff389aa1a918c8561ec
sittiborn Yooem <sittiporn.y@baanprogram.com>
2015-09-09 17:14:03 +0700

    ผู้เกษียนอายุ

--- welfare/apps/retire/service/ViewService.php
+++ welfare/apps/retire/service/ViewService.php
@@ -16,34 +16,32 @@ class ViewService extends CServiceBase implements IViewService {
         $this->datacontext = new CDataContext("default");
     }
 
-    
-
     public function retireList() {
         $view = new CJView("retire/lists", CJViewType::HTML_VIEW_ENGINE);
 
-        $sql = "SELECT concat(ti.titleNameTh,' ',re.registerNameTh,' ',re.registerLastNameTh) as fullname ,de.departmentNameTh,
-re.registerDateofBirth + interval 60 year as dateOut,
+        $sql = "SELECT  mem.fname,mem.lname, ti.value1 as titlename, ac.value1 as academic ,fac.value1 as faculty,de.value1 as department,
+mem.dob + interval 603 year as dateOut,
 CASE
-    WHEN re.registerDateAdded + interval 10 year >= curdate() THEN 10000
-    WHEN re.registerDateAdded + interval 15 year >= curdate() THEN 15000
-    WHEN re.registerDateAdded + interval 20 year >= curdate() THEN 20000
-    WHEN re.registerDateAdded + interval 25 year >= curdate() THEN 25000
-    WHEN re.registerDateAdded + interval 30 year >= curdate() THEN 30000
+    WHEN mem.workStartDate + interval 10 year >= curdate() THEN 10000
+    WHEN mem.workStartDate + interval 15 year >= curdate() THEN 15000
+    WHEN mem.workStartDate + interval 20 year >= curdate() THEN 20000
+    WHEN mem.workStartDate + interval 25 year >= curdate() THEN 25000
+    WHEN mem.workStartDate + interval 30 year >= curdate() THEN 30000
     ELSE NULL
 END AS amount
-FROM register re
-left join department de on re.departmentId = de.departmentId
-left join titlename ti on re.titleNameId = ti.titleNameId
-where re.registerDateofBirth + interval 60 year >= curdate() GROUP BY re.registerId";
-        
+FROM member mem
+left join taxonomy fac on mem.facultyId = fac.id
+left join taxonomy de on mem.departmentId = de.id
+left join taxonomy ti on mem.titleId = ti.id
+left join taxonomy ac on mem.academicId = ac.id
+where mem.dob + interval 60 year >= curdate() GROUP BY mem.memberId";
+
         $info = $this->datacontext->pdoQuery($sql);
         if ($info) {
             $view->retire = $info;
             return $view;
-        }
-        else{
+        } else {
             return $view;
-          
         }
     }
 
