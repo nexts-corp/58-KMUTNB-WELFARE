a3b7666921265e5fb33785a538d327c1a3e763ae
Pairat <Pairat@Pairat-PC>
2015-09-10 10:48:32 +0700

    แก้ js ไม่ให้ ดึงเสียง

--- welfare/assets/js/toastr/toastr.js
+++ welfare/assets/js/toastr/toastr.js
@@ -253,16 +253,16 @@
                 publish(response);
                 var PlaySound = 0;
                 PlaySound = 1;
-                if (isIE8orlower() == 0) {
-                    var audioElement = document.createElement("audio");
-                    audioElement.setAttribute("src", "assets/sound/alert.mp3");
-                    $.get();
-                    audioElement.addEventListener("load", function () {
-                        audioElement.play()
-                    }, true);
-                    audioElement.pause();
-                    audioElement.play()
-                }
+//                if (isIE8orlower() == 0) {
+//                    var audioElement = document.createElement("audio");
+//                    audioElement.setAttribute("src", "assets/sound/alert.mp3");
+//                    $.get();
+//                    audioElement.addEventListener("load", function () {
+//                        audioElement.play()
+//                    }, true);
+//                    audioElement.pause();
+//                    audioElement.play()
+//                }
 
                 if (options.debug && console) {
                     console.log(response);
