3f92a9e5527c398a9f93f168ae178e88ab9799a6
tawatchai <tawatchai@tawatchai-baanprogram>
2015-09-25 17:25:29 +0700

    น้องบี แก้ pdo เป็น getObject

--- welfare/apps/member/service/ViewService.php
+++ welfare/apps/member/service/ViewService.php
@@ -62,53 +62,34 @@ class ViewService extends CServiceBase implements IViewService {
         } else if ($usertype == "adminMedical") {
             $view = new CJView("user/editProfile", CJViewType::HTML_VIEW_ENGINE);
         }
-        $sql = "select mem1.fname,mem1.lname,mem1.idCard,mem1.memberId,(fac.value1) as faculty,(depart.value1) as department, "
-                . "aca.value1 as academic,mem1.titleNameId,title.value1 as titlename ,mem1.genderId,gende.value1 as gender,mem1.dob,mem1.employeeCode, "
-                . "mem1.employeeTypeId,mem1.facultyId,mem1.departmentId,mem1.positionId,mem1.matierId,mem1.internalPhone,mem1.academicId, "
-                . "mem1.phone,mem1.mobile,mem1.email,mem1.salaryDate,mem1.salary,emp.value1 as employeetype,psw.value1 as positionwork, "
-                . "mem1.address,mem1.workStartDate,mem1.workEndDate,mem1.memberActiveId,mat.value1 as matier "
-                . "FROM apps\\member\\model\\Member mem1 "
-                . "left JOIN apps\\taxonomy\\entity\\Taxonomy title "
-                . "with mem1.titleNameId = title.id "
-                . "left JOIN apps\\taxonomy\\entity\\Taxonomy gende "
-                . "with mem1.genderId = gende.id "
-                . "left JOIN apps\\taxonomy\\entity\\Taxonomy fac "
-                . "with mem1.facultyId = fac.id "
-                . "left JOIN apps\\taxonomy\\entity\\Taxonomy depart "
-                . "with mem1.departmentId = depart.id "
-                . "left JOIN apps\\taxonomy\\entity\\Taxonomy mat "
-                . "with mem1.matierId = mat.id "
-                . "left JOIN apps\\taxonomy\\entity\\Taxonomy aca "
-                . "with mem1.academicId = aca.id "
-                . "left JOIN apps\\taxonomy\\entity\\Taxonomy emp "
-                . "with mem1.employeeTypeId = emp.id "
-                . "left JOIN apps\\taxonomy\\entity\\Taxonomy psw "
-                . "with mem1.positionId = psw.id "
-                . "WHERE mem1.memberId=$id ";
+        $sql = "select mem1 "
+                . "FROM apps\\member\\model\\FullMember mem1 "
+                . "WHERE mem1.memberActive2 = 'Working' and mem1.memberId=:id ";
 
-        $member = $this->datacontext->getObject($sql);
+         $param["id"] = $id;
+        $member = $this->datacontext->getObject($sql,$param);
 //            $Y=Date("Y")+543;
 //        print_r($member);
 //        exit();
-        $dob = $member[0]['dob']->format('d-m-Y');
+//        $dob = $member[0]['dob']->format('d-m-Y');
 
 //        print_r($member);
 
-        $mem = explode("-", $dob);
-        $member[0]['dob'] = $mem[0] . "-" . $mem[1] . "-" . (intval($mem[2]) + 543);
+        $mem = explode("-", $member[0]->dob);
+       $member[0]->dob = $mem[2] . "-" . $mem[1] . "-" . (intval($mem[0]) + 543);
 
-        $workStartDate = $member[0]['workStartDate']->format('d-m-Y');
-        $wsd = explode("-", $workStartDate);
-        $member[0]['workStartDate'] = $wsd[0] . "-" . $wsd[1] . "-" . (intval($wsd[2]) + 543);
+//        $workStartDate = $member[0]['workStartDate']->format('d-m-Y');
+        $wsd = explode("-", $member[0]->workStartDate);
+        $member[0]->workStartDate = $wsd[2] . "-" . $wsd[1] . "-" . (intval($wsd[0]) + 543);
 
-        $salaryDate = $member[0]['salaryDate']->format('d-m-Y');
-        $wsd = explode("-", $salaryDate);
-        $member[0]['salaryDate'] = $wsd[0] . "-" . $wsd[1] . "-" . (intval($wsd[2]) + 543);
+//        $salaryDate = $member[0]['salaryDate']->format('d-m-Y');
+        $wsd = explode("-", $member[0]->salaryDate);
+        $member[0]->salaryDate= $wsd[2] . "-" . $wsd[1] . "-" . (intval($wsd[0]) + 543);
 
         $user = new \apps\user\entity\User();
-        $user->memberId = $member[0]['memberId'];
+        $user->memberId = $member[0]->memberId;
         $user = $this->datacontext->getObject($user)[0];
-        $member[0]['userTypeId'] = $user->userTypeId;
+        $member[0]->userTypeId = $user->userTypeId;
         $view->datas = $member;
 
         $academic = new Taxonomy();
@@ -158,8 +139,8 @@ class ViewService extends CServiceBase implements IViewService {
         $datafilter = $this->getRequest();
 
         $param = array();
-        $sql = "select * ,IFNULL(mem1.academic1,mem1.titleName1) title "
-                . "FROM v_fullmember mem1 "
+        $sql = "select mem1 "
+                . "FROM apps\\member\\model\\FullMember mem1 "
                 . "WHERE mem1.memberActive2 = 'Working'  ";
 
         if ($usertype == "administrator") {
@@ -185,60 +166,43 @@ class ViewService extends CServiceBase implements IViewService {
             $filter = new MemberService();
             $view->lists = $filter->search($datafilter);
         } else {
-            $view->lists = $this->datacontext->pdoQuery($sql, $param); //กรณีที่ไม่ได้ search
+            $view->lists = $this->datacontext->getObject($sql, $param); //กรณีที่ไม่ได้ search
         }
         return $view;
     }
 
     public function memberShow($id) {
         $view = new CJView("user/profile", CJViewType::HTML_VIEW_ENGINE);
-        $sql = "select mem1.fname,mem1.lname,mem1.idCard,mem1.memberId,(fac.value1) as faculty,(depart.value1) as department, "
-                . "aca.value1 as academic,mem1.titleNameId,title.value1 as titlename ,mem1.genderId,gende.value1 as gender,mem1.dob,mem1.employeeCode, "
-                . "mem1.employeeTypeId,mem1.facultyId,mem1.departmentId,mem1.positionId,mem1.matierId,mem1.internalPhone,mem1.academicId, "
-                . "mem1.phone,mem1.mobile,mem1.email,mem1.salaryDate,mem1.salary,emp.value1 as employeetype,psw.value1 as positionwork, "
-                . "mem1.address,mem1.workStartDate,mem1.workEndDate,mem1.memberActiveId,mat.value1 as matier "
-                . "FROM apps\\member\\model\\Member mem1 "
-                . "left JOIN apps\\taxonomy\\entity\\Taxonomy title "
-                . "with mem1.titleNameId = title.id "
-                . "left JOIN apps\\taxonomy\\entity\\Taxonomy gende "
-                . "with mem1.genderId = gende.id "
-                . "left JOIN apps\\taxonomy\\entity\\Taxonomy fac "
-                . "with mem1.facultyId = fac.id "
-                . "left JOIN apps\\taxonomy\\entity\\Taxonomy depart "
-                . "with mem1.departmentId = depart.id "
-                . "left JOIN apps\\taxonomy\\entity\\Taxonomy mat "
-                . "with mem1.matierId = mat.id "
-                . "left JOIN apps\\taxonomy\\entity\\Taxonomy aca "
-                . "with mem1.academicId = aca.id "
-                . "left JOIN apps\\taxonomy\\entity\\Taxonomy emp "
-                . "with mem1.employeeTypeId = emp.id "
-                . "left JOIN apps\\taxonomy\\entity\\Taxonomy psw "
-                . "with mem1.positionId = psw.id "
-                . "WHERE mem1.memberId=$id ";
+        $sql = "select mem1 "
+                . "FROM apps\\member\\model\\FullMember mem1 "
+                . "WHERE mem1.memberActive2 = 'Working' and mem1.memberId=:id ";
 
-        $member = $this->datacontext->getObject($sql);
+         $param["id"] = $id;
+        $member = $this->datacontext->getObject($sql,$param);
 //            $Y=Date("Y")+543;
 //        exit();
-        $dob = $member[0]['dob']->format('d-m-Y');
+//        print_r($member);
+//        exit();
+        //$dob = $member->dob->format('d-m-Y');
 
 //        print_r($member);
 
-        $mem = explode("-", $dob);
-        $member[0]['dob'] = $mem[0] . "-" . $mem[1] . "-" . (intval($mem[2]) + 543);
+        $mem = explode("-", $member[0]->dob);
+       $member[0]->dob = $mem[2] . "-" . $mem[1] . "-" . (intval($mem[0]) + 543);
 
-        $workStartDate = $member[0]['workStartDate']->format('d-m-Y');
-        $wsd = explode("-", $workStartDate);
-        $member[0]['workStartDate'] = $wsd[0] . "-" . $wsd[1] . "-" . (intval($wsd[2]) + 543);
+//        $workStartDate = $member[0]['workStartDate']->format('d-m-Y');
+        $wsd = explode("-", $member[0]->workStartDate);
+        $member[0]->workStartDate = $wsd[2] . "-" . $wsd[1] . "-" . (intval($wsd[0]) + 543);
 
-        $salaryDate = $member[0]['salaryDate']->format('d-m-Y');
-        $wsd = explode("-", $salaryDate);
-        $member[0]['salaryDate'] = $wsd[0] . "-" . $wsd[1] . "-" . (intval($wsd[2]) + 543);
+//        $salaryDate = $member[0]['salaryDate']->format('d-m-Y');
+        $wsd = explode("-", $member[0]->salaryDate);
+        $member[0]->salaryDate= $wsd[2] . "-" . $wsd[1] . "-" . (intval($wsd[0]) + 543);
 //        print_r($member);
 //        exit();
         $user = new \apps\user\entity\User();
-        $user->memberId = $member[0]['memberId'];
+        $user->memberId = $member[0]->memberId;
         $user = $this->datacontext->getObject($user)[0];
-        $member[0]['userTypeId'] = $user->userTypeId;
+        $member[0]->userTypeId = $user->userTypeId;
         $academic = new Taxonomy();
         $academic->pCode = "academic";
         $view->academic = $this->datacontext->getObject($academic);
