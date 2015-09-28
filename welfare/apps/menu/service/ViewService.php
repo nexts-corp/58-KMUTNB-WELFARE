936060fe3592ebc889be6324a862f48966c9d6fc
Pairat Plangdee <pairat.p@baanprogram.com>
2015-09-28 19:59:50 +0700

    เพิ่ม nottifications บน header menu

--- welfare/apps/menu/service/ViewService.php
+++ welfare/apps/menu/service/ViewService.php
@@ -7,6 +7,7 @@ use th\co\bpg\cde\data\CDataContext;
 use th\co\bpg\cde\collection\CJView;
 use th\co\bpg\cde\collection\CJViewType;
 use apps\menu\interfaces\IViewService;
+use apps\common\entity\Nottifications;
 
 class ViewService extends CServiceBase implements IViewService {
 
@@ -18,10 +19,33 @@ class ViewService extends CServiceBase implements IViewService {
 
     public function header() {
         $view = new CJView("header", CJViewType::HTML_VIEW_ENGINE);
-        $view->code = $this->getCurrentUser()->code;
+
+        $code = $this->getCurrentUser()->code;
+        $view->code = $code;
         $view->profile = $this->getCurrentUser()->name;
         $view->userType = $this->getCurrentUser()->usertype;
         $view->titleName = $this->getCurrentUser()->titleName;
+
+        $pathNft = "apps\\common\\entity\\";
+
+        $sqlNft = "SELECT nft.memberId, nft.nftName,nft.nftAppName,nft.nftStatus,nft.nftLink,nft.nftAppId,nft.dateCreated "
+                . " From " . $pathNft . "Nottifications nft where nft.nftStatus='false' And nft.memberId =" . $code . "  ";
+
+        $objNft = $this->datacontext->getObject($sqlNft);
+        if ($objNft != "") {
+            foreach ($objNft as $key => $value) {
+                $objNft[$key]["dateCreated"] = $value["dateCreated"]->format("d-m-Y");
+            }
+        }
+        $view->datasNft = $objNft;
+
+        $i = 0;
+        foreach ($objNft as $key => $value) {
+            $objNft[$key] = $i++;
+        }
+
+        $view->total = $i--;
+
         return $view;
     }
 
