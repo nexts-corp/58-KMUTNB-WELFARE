2ec50f0b05a5df0893c514e712a96922b8d50f16
Panya Noipram <panya.n@baanprogram.com>
2016-02-05 14:15:53 +0700

    api

--- welfare/api.php
+++ welfare/api.php
@@ -3,7 +3,7 @@ date_default_timezone_set('Asia/Bangkok');
 require_once "vendor/autoload.php";
 require_once "vendor/mustache/mustache/src/Mustache/Autoloader.php";
 require_once 'vendor/bpg/cde/lib/core/AutoLoader.php';
-require_once 'vendor/phpoffice/tcpdf/tcpdf.php';
+
 use th\co\bpg\cde\core\ChangdaoEngine;
 Logger::configure(dirname(__FILE__) . '/log4php.properties');
 Mustache_Autoloader::register();
